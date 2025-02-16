ad3059728c8a85f584bf3f028c43e55df470a6a7
Sushanto.171 <sushantochandrasharkar171@gmail.com>
2025-02-16 21:27:05 +0600

    solved 1st problem

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-const s = "madam";
-let new_str = s.split("").reduce((prev, current) => current + prev, "");
+// const s = "madam";
+// let new_str = s.split("").reduce((prev, current) => current + prev, "");
 
-const isPalindrome = new_str === s ? "Yes" : "No";
-console.log(isPalindrome);
+// const isPalindrome = new_str === s ? "Yes" : "No";
+// console.log(isPalindrome);
+// const arr = [-4, 3, -9, 0, 4, 1];
+// const n = 6;
+// let positive = [];
+// let negative = [];
+// let zero = [];
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] === 0) {
+//     zero.push(arr[i]);
+//   } else if (arr[i] < 0) {
+//     negative.push(arr[i]);
+//   } else if (arr[i] > 0) {
+//     positive.push(arr[i]);
+//   }
+// }
+// console.log((positive.length / n).toFixed(6));
+// console.log((negative.length / n).toFixed(6));
+// console.log((zero.length / n).toFixed(6));
